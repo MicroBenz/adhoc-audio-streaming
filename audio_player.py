dea24aa3e887bf77cea9f7a8c3bfc7c97b6f1811
Tananan Tangthanachaikul <microbenz.prob@gmail.com>
2016-11-07 20:26:32 +0700

    Work from today

--- audio_player.py
+++ audio_player.py
@@ -2,34 +2,49 @@ import pyaudio
 import wave
 import sys
 
-CHUNK = 1024
+CHUNK_SIZE = 1024
 
 if len(sys.argv) < 2:
     print("Plays a wave file.\n\nUsage: %s filename.wav" % sys.argv[0])
     sys.exit(-1)
 
+# READ WAVE FILE
 wf = wave.open(sys.argv[1], 'rb')
 
 # instantiate PyAudio (1)
 p = pyaudio.PyAudio()
 
-# open stream (2)
+# open PyAudio stream (2)
 stream = p.open(format=p.get_format_from_width(wf.getsampwidth()),
                 channels=wf.getnchannels(),
                 rate=wf.getframerate(),
                 output=True)
 
-# read data
-data = wf.readframes(CHUNK)
+print wf.getsampwidth()
+print wf.getnchannels()
+print wf.getframerate()
 
-# play stream (3)
-while len(data) > 0:
-    stream.write(data)
-    data = wf.readframes(CHUNK)
+while True:
+    chunk = wf.readframes(CHUNK_SIZE)
+    if not chunk:
+        print 'LAST CHUNK'
+        break
+    else:
+        stream.write(chunk)
 
-# stop stream (4)
-stream.stop_stream()
-stream.close()
 
-# close PyAudio (5)
-p.terminate()
+
+# # read data
+# data = wf.readframes(CHUNK)
+
+# # play stream (3)
+# while len(data) > 0:
+#     stream.write(data)
+#     data = wf.readframes(CHUNK)
+
+# # stop stream (4)
+# stream.stop_stream()
+# stream.close()
+
+# # close PyAudio (5)
+# p.terminate()
