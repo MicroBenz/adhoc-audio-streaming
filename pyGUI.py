cca6680fc3198a3a826c9c21c4cdd98edb290337
Pitarn Harnpanichpun <pitarnh@outlook.com>
2016-11-11 16:40:15 +0700

    GUI: GUI can choose music and run script

--- pyGUI.py
+++ pyGUI.py
@@ -11,24 +11,26 @@ class simpleApp_TK(Tkinter.Tk):
 
 	def initialize(self):
 		self.grid()
-		entryText=""
 		global entryText
+		entryText=""
 		entryText=Tkinter.StringVar()
 		self.entry=Tkinter.Entry(self,textvariable=entryText)
 		self.entry.grid(column=0,row=0,sticky="EW")
 		self.entry.focus_set()
 
-		button=Tkinter.Button(self,text=u"Browse for musics.",command=self.OnButtonClick)
+		button=Tkinter.Button(self,text=u"Browse for musics...",command=self.OnButtonClick)
 		button.grid(column=1,row=0,sticky="EW")
 
-		button=Tkinter.Button(self,text=u"Play",command="")
-		button.grid(column=3,row=3)
-		button.place(relx=0.5,rely=0.5,anchor="center")
+		
+		button1=Tkinter.Button(self,text=u"Play",command=self.playMusic)
+		button1.grid(column=3,row=3)
+		button1.place(relx=0.5,rely=0.5,anchor="center")
 		
 
 
 		self.grid_columnconfigure(0,weight=1)
-		self.resizable(False,True)
+		self.resizable(False,False)
+
 
 	
 	def OnButtonClick(self):
@@ -39,12 +41,14 @@ class simpleApp_TK(Tkinter.Tk):
 			filename=os.path.basename(path)
 			entryText.set(filename)
 		
-		
+	def playMusic(self):
+		os.system('python client.py '+self.entry.get())		
 
         	        
 
 if __name__=="__main__":
 	app=simpleApp_TK(None)
 	app.title('Wireless speaker')
+	app.geometry('{}x{}'.format(500,500))
 	app.mainloop()
 
